e83fb4d1113a60bddd6d9d2d28f85b0911ccf9e3
Haider Zaman <106913229+haideryzai@users.noreply.github.com>
2025-03-19 04:10:37 +0500

    Create 1-BasicJavaScriptExecution.js

---
+++ 1-BasicJavaScriptExecution.js
@@ -0,0 +1,8 @@
+// This is a simple script that shows a message
+alert("Hello, world!");
+
+// What happens:
+// 1. You load this in a webpage.
+// 2. The JavaScript engine reads this line with an interpreter.
+// 3. It translates "alert" into machine code to show a popup.
+// 4. Since this runs only once, the engine doesn’t bother compiling it—it’s fast enough!
